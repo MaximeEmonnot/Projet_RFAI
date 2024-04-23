01e8a704a68c58cfe846acfe793dfe60dc44e67e
Maxime Emonnot <61348720+MaximeEmonnot@users.noreply.github.com>
2024-04-24 01:22:15 +0200

    Réorganisation du projet + Tests

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,11 +1,13 @@
 #include <EngineException.h>
-#include <Image.h>
-#include <Transformations.h>
-#include <Window.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <filesystem>
 
-void Test()
+// FONCTIONS TESTS DE TRAITEMENT
+
+void TestHistogram(std::string const& path)
 {
-    cv::Mat src = cv::imread("Assets/IMG_0225.jpg");
+    cv::Mat src = cv::imread(path);
 
 	std::vector<cv::Mat> bgr_planes;
     split(src, bgr_planes);
@@ -40,27 +42,191 @@ void Test()
 			cv::Scalar(0, 0, 255), 2, 8, 0);
     }
 
-	namedWindow("Source image", cv::WINDOW_AUTOSIZE);
 	imshow("Source image", src);
-	namedWindow("calcHist Demo", cv::WINDOW_AUTOSIZE);
     imshow("calcHist Demo", histImage);
 
 	cv::imwrite("Outputs/Histogram.png", histImage);
 	cv::waitKey();
 }
 
-void Safe()
+void TestHistogramGrayscale(std::string const& path)
+{
+	cv::Mat src = cv::imread(path, cv::IMREAD_GRAYSCALE);
+
+	int histSize = 256;
+	float range[] = { 0, 256 };
+	const float* histRange[] = { range };
+
+	cv::Mat hist;
+	cv::calcHist(&src, 1, 0, cv::Mat(), hist, 1, &histSize, histRange);
+
+	int hist_w = 512, hist_h = 400;
+	cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
+	normalize(hist, hist, 0, 255, cv::NORM_MINMAX, -1, cv::Mat());
+
+	for(int i = 0; i < histSize; i++) 
+	{
+		float binVal = hist.at<float>(i);
+		int intensity = static_cast<int>(binVal * hist_h / 255);
+		cv::line(histImage, cv::Point(i, hist_h), cv::Point(i, hist_h - intensity), cv::Scalar(255, 255, 255));
+	}
+
+	imshow("Source image", src);
+	imshow("calcHist Demo", histImage);
+
+	cv::waitKey();
+}
+
+void TestContour(std::string const& path)
+{
+	cv::Mat src = cv::imread(path, cv::IMREAD_GRAYSCALE);
+	cv::Mat edges;
+
+	cv::Canny(src, edges, 50, 150);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(edges, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+	if(!contours.empty())
+	{
+		cv::Rect r = cv::boundingRect(contours.at(0));
+		cv::Mat roi = src(r).clone();
+		cv::imshow("Object", roi);
+	}
+	cv::waitKey(0);
+}
+
+void TestBlobDetection(std::string const& path)
+{
+	cv::Mat src = cv::imread(path);
+
+	cv::Mat dst;
+	cv::cvtColor(src, dst, cv::COLOR_BGR2HSV);
+	cv::cvtColor(dst, src, cv::COLOR_BGR2GRAY);
+
+	cv::SimpleBlobDetector::Params params;
+	// Thresholds
+	params.minThreshold = 1.f;
+	params.maxThreshold = 255.f;
+
+	// Filter by Area
+	params.filterByArea = true;
+	params.minArea      = 1000.f;
+
+	// Filter by Circularity
+	params.filterByCircularity = true;
+	params.minCircularity      = 0.1f;
+
+	// Filter by Convexity
+	params.filterByConvexity = true;
+	params.minConvexity      = 0.75f;
+
+	// Filter by Inertia
+	params.filterByInertia = true;
+	params.minInertiaRatio = 0.00000001f;
+
+	// Filter by Color
+	params.filterByColor = false;
+	params.blobColor     = 0;
+
+	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+
+	std::vector<cv::KeyPoint> keyPoints;
+	detector->detect(src, keyPoints);
+
+	std::cout << "Keypoints : " << keyPoints.size() << std::endl;
+
+	cv::Mat imgKeypoints;
+	cv::drawKeypoints(src, keyPoints, imgKeypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	cv::imshow("Keypoints", imgKeypoints);
+	cv::waitKey(0);
+}
+
+void TestEdgeDetection(std::string const& path)
+{
+	cv::Mat img = cv::imread(path);
+
+	cv::Mat img_gray;
+	cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+
+	cv::Mat img_blur;
+	cv::GaussianBlur(img_gray, img_blur, cv::Size(3, 3), 0);
+
+	cv::Mat sobelx, sobely, sobelxy;
+	cv::Sobel(img_blur,  sobelx, CV_64F, 1, 0, 5);
+	cv::Sobel(img_blur,  sobely, CV_64F, 0, 1, 5);
+	cv::Sobel(img_blur, sobelxy, CV_64F, 1, 1, 5);
+
+	cv::imshow("Sobel X", sobelx);
+	cv::imshow("Sobel Y", sobely);
+	cv::imshow("Sobel XY (Sobel() Function)", sobelxy);
+
+	cv::Mat edges;
+	cv::Canny(img_blur, edges, 100, 200, 3, false);
+
+	cv::imshow("Canny edge detection", edges);
+
+	cv::waitKey(0);
+}
+
+void TestThresholding(std::string const& path)
+{
+	cv::Mat src = cv::imread(path, cv::IMREAD_GRAYSCALE);
+	cv::Mat dst;
+
+	cv::threshold(src, dst, 150, 255, cv::THRESH_BINARY);
+
+	cv::imshow("Threshold", dst);
+
+	cv::waitKey(0);
+}
+
+void TestColorspaces(std::string const& path)
+{
+	cv::Mat src = cv::imread(path);
+
+	cv::Mat dst;
+	cv::cvtColor(src, dst, cv::COLOR_BGR2HSV);
+
+	cv::imshow("Colorspace", dst);
+
+	cv::waitKey(0);
+}
+
+// FIN FONCTIONS TESTS DE TRAITEMENT
+
+void RunTests(std::string const& imagePath)
+{
+	TestBlobDetection(imagePath);
+}
+
+// MAIN : NE PAS MODIFIER
+int main()
 {
 	try
 	{
-		Image  const image("Assets/IMG_0212.jpg");
-		Window const window("Original Window");
+		while(true)
+		{
+			// Sélection de l'image à traiter
+			std::vector<std::filesystem::path> files{};
+			int fileChoice = -1;
+			while(fileChoice == -1)
+			{
+				files.clear();
+				for(const auto& entry : std::filesystem::directory_iterator("Assets"))
+					files.emplace_back(entry.path());
+				std::wcout << "Selectionnez votre image a traiter : " << std::endl;
+				for(size_t i = 0; i < files.size(); ++i)
+					std::cout << " " << i << " - " << files.at(i) << std::endl;
+				std::cout << "-1 - Rafraichir la liste" << std::endl;
+				std::cin >> fileChoice;
+			}
+			std::cout << "Exécution des tests..." << std::endl;
 
-		Image  const filter = Transformations::MaskChannel(image, NONE);
-		Window const filterWindow("Filter Window");
-
-		window.DisplayImage(image);
-		filterWindow.DisplayImage(filter);
+			// Exécution des tests de traitement
+			RunTests(files.at(fileChoice).string());
+		}
 	}
 	catch(EngineException const& e)
 	{
@@ -74,15 +240,6 @@ void Safe()
 	{
 		EngineException::DisplayMessageBox(L"Unknown Exception", L"An unknown exception has occurred!");
 	}
-}
-
-int main()
-{
-	// Commenter l'un ou l'autre
-	// Test : Tous les bouts de code qu'on souhaiterais tester à part, sans se poser de question concernant une bonne structure ou non
-	// Safe : Code bien structuré
-	// Test();
-	Safe();
 
 	return 0;
 }
